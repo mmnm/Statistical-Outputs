090d7284d8e0857b6186dd3695a95fe71f5532a3
Manu Mishra <manu.mish@gmail.com>
2015-06-12 13:02:53 -0700

    Remaining initial SQL/ supported JSON parsing

---
+++ demoweb/script.js
@@ -0,0 +1,13 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Manu Mi. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='sucks.html'>Sucks</a></li>" +
+"</ul>";
+
+
+
